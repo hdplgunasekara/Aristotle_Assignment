ec20039399c630d5ef833a197000e2d276dc7c9c
Pasindu Lakshan Gunasekara <66919299+hdplgunasekara@users.noreply.github.com>
2023-04-12 21:12:43 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configureApp(app);
-  await app.listen(3001);
+  await app.listen(process.env.PORT|| 3000 );
 }
 
 function configureApp(app: INestApplication) {
